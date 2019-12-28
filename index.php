5ff8561c4c0369f4a5ffe0ffb70704b48c9935f7
Naman Kaushik <naman.kaushik@timesinternet.in>
2019-12-28 17:52:54 +0530

    formatting od

--- index.php
+++ index.php
@@ -1,101 +1,115 @@
 <?php
-  include "input.php";
-  $start_time = microtime(true); 
-  $input=(string) $input;
-  $arr=explode(",",$input);
-  $babCount=0;
-  $delimeter='@@$$@@';
-  foreach($arr as $key=>$thisVal){
-	$cleanArr=[];
-	$enclosedStrings=[];
-	if (preg_match_all("/\[([^\]]*)\]/", $thisVal, $matches)) {
-		$enclosedStrings=$matches[1];
-		$cleanArr['inside_str']=$enclosedStrings;
-	}
+include "input.php";
+$start_time = microtime(true);
+$input = (string)$input;
+$arr = explode(",", $input);
+$babCount = 0;
+$delimeter = '@@$$@@';
+foreach ($arr as $key => $thisVal)
+{
+    $cleanArr = [];
+    $enclosedStrings = [];
+    if (preg_match_all("/\[([^\]]*)\]/", $thisVal, $matches))
+    {
+        $enclosedStrings = $matches[1];
+        $cleanArr['inside_str'] = $enclosedStrings;
+    }
 
-	$cleanStr=preg_replace( '~\[.*\]~' , $delimeter, $thisVal);  
-	$pos = strpos($cleanStr, $delimeter);
-    if($pos !== false) {
-		$cleanArr['outside_str']=explode($delimeter,$cleanStr);
-	}
-	else{
-		$cleanArr['outside_str']=$thisVal; //no square brackets
-	}
+    $cleanStr = preg_replace('~\[.*\]~', $delimeter, $thisVal);
+    $pos = strpos($cleanStr, $delimeter);
+    if ($pos !== false)
+    {
+        $cleanArr['outside_str'] = explode($delimeter, $cleanStr);
+    }
+    else
+    {
+        $cleanArr['outside_str'] = $thisVal; //no square brackets
+        
+    }
 
-		$isBaab=checkSubString($cleanArr);
-		if(!empty($isBaab) && $isBaab!==false){
-			$babCount++;
-	}
-
-  }
-  $end_time = microtime(true); 
-  $execution_time = ($end_time - $start_time);   
-  echo "Total valid Unique Ids found ".$babCount."<br>";
-  echo " Execution time = ".round($execution_time,2)." sec"; 
-?>
-<?php
-
-   function validateBaab($str='') {
-    $first_half=substr($str,0,2);
-	$second_half=strrev(substr($str,-2));
-	return $first_half===$second_half;
-	}
- 
-  function checkSubString($strArr)  
-{ 
-	//check if square bracket enclosed strings contains a baab
-	$inside_str=isset($strArr['inside_str'])?$strArr['inside_str']:[];
-	$outside_str=$strArr['outside_str'];
-	$isBaabWithinBrackets=false;
-	if(!empty($inside_str)){
-		$insideBaabStr=checkBaab($inside_str);
-		$isBaabWithinBrackets=!empty($insideBaabStr)?true:false;
-		if($isBaabWithinBrackets===true){return false;} //returnig if square bracket contains baab
-	}
-	
-	if(!empty($outside_str)){
-		$isoutsideBaabStr=checkBaab($outside_str);
-		$isBaabOutside=!empty($isoutsideBaabStr)?true:false;
-		return $isBaabOutside;
-
-	}
+    $isBaab = checkSubString($cleanArr);
+    if (!empty($isBaab) && $isBaab !== false)
+    {
+        $babCount++;
+    }
 
 }
-function checkBaab($strArr){
-	$isBaab=false;
-	foreach($strArr as $str){
-	
-		$n=strlen($str);
-		for ($len = 1; $len <= $n; $len++)  
-		{  
-			//end          
-			for ($i = 0; $i <= $n - $len; $i++)  
-			{               
-				$j = $i + $len - 1;   
-				$subLength=($j-$i+1);
-				
-	
-				if($subLength<4 || $subLength>4){
-					//echo 'kaushik<br>';
-					continue; //escaping loop in case string length don't match baab length i.e. 4
-				}
-	
-				$sub=''; 
-				for ($k = $i; $k <= $j; $k++) {
-					$sub.=$str[$k]; 
-				}
-				//echo 'counter'.$i."==".$j."==".$sub."<br>";      
+$end_time = microtime(true);
+$execution_time = ($end_time - $start_time);
+echo "Total valid Unique Ids found " . $babCount . "<br>";
+echo " Execution time = " . round($execution_time, 2) . " sec";
+?>
+<?php
+function validateBaab($str = '')
+{
+    $first_half = substr($str, 0, 2);
+    $second_half = strrev(substr($str, -2));
+    return $first_half === $second_half;
+}
 
-				//secho 'naman<br>';
-	
-				if(!empty($sub) && validateBaab($sub)===true){  
-					return true;
-				}
-			}             
-	
-		} 
-	}
-  
-	return $isBaab;
+function checkSubString($strArr)
+{
+    //check if square bracket enclosed strings contains a baab
+    $inside_str = isset($strArr['inside_str']) ? $strArr['inside_str'] : [];
+    $outside_str = $strArr['outside_str'];
+    $isBaabWithinBrackets = false;
+    if (!empty($inside_str))
+    {
+        $insideBaabStr = checkBaab($inside_str);
+        $isBaabWithinBrackets = !empty($insideBaabStr) ? true : false;
+        if ($isBaabWithinBrackets === true)
+        {
+            return false;
+        } //returnig if square bracket contains baab
+        
+    }
+
+    if (!empty($outside_str))
+    {
+        $isoutsideBaabStr = checkBaab($outside_str);
+        $isBaabOutside = !empty($isoutsideBaabStr) ? true : false;
+        return $isBaabOutside;
+
+    }
+
+}
+function checkBaab($strArr)
+{
+    $isBaab = false;
+    foreach ($strArr as $str)
+    {
+
+        $n = strlen($str);
+        for ($len = 1;$len <= $n;$len++)
+        {
+            //end
+            for ($i = 0;$i <= $n - $len;$i++)
+            {
+                $j = $i + $len - 1;
+                $subLength = ($j - $i + 1);
+
+                if ($subLength < 4 || $subLength > 4)
+                {
+                    //echo 'kaushik<br>';
+                    continue; //escaping loop in case string length don't match baab length i.e. 4
+                    
+                }
+
+                $sub = '';
+                for ($k = $i;$k <= $j;$k++)
+                {
+                    $sub .= $str[$k];
+                }
+                //echo 'counter'.$i."==".$j."==".$sub."<br>";
+                //secho 'naman<br>';
+                if (!empty($sub) && validateBaab($sub) === true)
+                {
+                    return true;
+                }
+            }
+
+        }
+    }
+    return $isBaab;
 }
 ?>
